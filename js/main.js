abd9c4f23773563eaf86f7ef292e4fbab91dd4e8
aleballabio <aleballabio05@gmail.com>
2022-03-03 18:29:22 +0100

    finished + first bonus

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ function play() {
     grid.innerHTML = ""
     grid.style.display = "flex";
     let difficultyValue = difficulty.value;
-    const finalCellsNumber = []
     let score = 0;
+    let NumeriBombe = 0;
 
     //Difficutly of the Game
 
@@ -25,18 +25,34 @@ function play() {
 
         case "Facile":
             score = 0
-            const NumeriBombe = generazioneBombe(49);
+            NumeriBombe = generazioneBombe(49);
             console.log(NumeriBombe);
+            arrCells = [];
 
             for (let times = 1; times <= 49; times++) {
 
                 const ElementCell = document.createElement("div");
                 ElementCell.classList.add("cell");
                 ElementCell.classList.add("small");
+
                 ElementCell.addEventListener("click", function () {
-                    ElementCell.style.backgroundColor = "green";
-                    score++;
-                    console.log(score);
+
+                    if (NumeriBombe.includes(times)) {
+                        ElementCell.style.backgroundColor = "red";
+                        if (alert("Hai Perso! Ma hai totalizzato" + " " + score + " " + "punti!")) { }
+
+                        else {
+                            ElementCell.style.backgroundColor = "red";
+                            grid.style.display = "none";
+
+                        }
+                    }
+
+                    else {
+                        ElementCell.style.backgroundColor = "green";
+                        score++;
+                        ElementCell.style.pointerEvents = "none";
+                    }
                 });
 
                 grid.append(ElementCell);
@@ -47,15 +63,35 @@ function play() {
             break;
 
         case "Medio":
+            score = 0
+            NumeriBombe = generazioneBombe(81);
+            console.log(NumeriBombe);
+            arrCells = [];
+
             for (let times = 1; times <= 81; times++) {
+
                 const ElementCell = document.createElement("div");
                 ElementCell.classList.add("cell");
-                ElementCell.classList.add("medium");
+                ElementCell.classList.add("small");
 
-                ElementCell.style.flexBasis = "calc(100% / 9)";
-                ElementCell.style.height = "calc(100% / 9)";
                 ElementCell.addEventListener("click", function () {
-                    ElementCell.classList.add("change-color");
+
+                    if (NumeriBombe.includes(times)) {
+                        ElementCell.style.backgroundColor = "red";
+                        if (alert("Hai Perso! Ma hai totalizzato" + " " + score + " " + "punti!")) { }
+
+                        else {
+                            ElementCell.style.backgroundColor = "red";
+                            grid.style.display = "none";
+
+                        }
+                    }
+
+                    else {
+                        ElementCell.style.backgroundColor = "green";
+                        score++;
+                        ElementCell.style.pointerEvents = "none";
+                    }
                 });
 
                 grid.append(ElementCell);
@@ -66,13 +102,35 @@ function play() {
             break;
 
         case "Difficile":
-            for (let times = 1; times <= 100; times++) {
-                const ElementCell = document.createElement("div");
+            score = 0
+            NumeriBombe = generazioneBombe(100);
+            console.log(NumeriBombe);
+            arrCells = [];
 
+            for (let times = 1; times <= 100; times++) {
+
+                const ElementCell = document.createElement("div");
                 ElementCell.classList.add("cell");
-                ElementCell.classList.add("big");
+                ElementCell.classList.add("small");
+
                 ElementCell.addEventListener("click", function () {
-                    ElementCell.classList.add("change-color");
+
+                    if (NumeriBombe.includes(times)) {
+                        ElementCell.style.backgroundColor = "red";
+                        if (alert("Hai Perso! Ma hai totalizzato" + " " + score + " " + "punti!")) { }
+
+                        else {
+                            ElementCell.style.backgroundColor = "red";
+                            grid.style.display = "none";
+
+                        }
+                    }
+
+                    else {
+                        ElementCell.style.backgroundColor = "green";
+                        score++;
+                        ElementCell.style.pointerEvents = "none";
+                    }
                 });
 
                 grid.append(ElementCell);
